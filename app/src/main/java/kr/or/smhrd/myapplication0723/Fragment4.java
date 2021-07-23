79980e9e2a6e9e6de28fc1142704022262172955
woyeonggwang <zerolight9@naver.com>
2021-07-23 17:38:45 +0900

    커밋한다아아아ㅏ

--- app/src/main/java/kr/or/smhrd/myapplication0723/Fragment4.java
+++ app/src/main/java/kr/or/smhrd/myapplication0723/Fragment4.java
@@ -15,7 +15,7 @@ public class Fragment4 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragmentffff
 
         View view = inflater.inflate(R.layout.fragment_4, container, false);
 
